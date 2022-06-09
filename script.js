2363ab5a435218b78e21bf918cd30ef41599bbe5
Jolene Kearse <70668540+JoleneKearse@users.noreply.github.com>
2022-06-09 14:40:02 -0400

    Final refactoring complete

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ emailCollectorForm.addEventListener('submit', (event) => {
     let myFormData = new FormData(event.target)
     let userName = myFormData.get('name')
     let userEmail = myFormData.get('email')
-    const updatedHtmlContent = `
+    let updatedHtmlContent = `
         <h2 class="form-title congrats">Congratulations, ${userName}!</h2>
         <img src="grill.png" alt="" class="gap">
         <p class="form-cta accent-txt uppercase">You're on your way to becoming a BBQ Master!</p>
@@ -16,4 +16,15 @@ emailCollectorForm.addEventListener('submit', (event) => {
     congratsMsg.classList.add('congrats')
     congratsStatement.classList.remove('accent-txt')
     signUpComplete.innerHTML = updatedHtmlContent
+})
+
+// reset the form on reload
+document.getElementById('body').addEventListener('load', function() {
+    let updatedForm = `
+        <form id="emailCollector" class="flex" action="#" method="post">
+            <input type="text" name="name" id="name" aria-label="name" placeholder="first name" required>
+            <input type="text" name="email" id="email" aria-label="email" placeholder="email" required>
+            <button type="submit" class="uppercase cta-btn">Register</button>
+        </form>`
+    emailCollectorForm.innerHTML = updatedForm
 })
